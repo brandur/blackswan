db14153a98642ca1e910826de5907bf4c8875ef6
Brandur <brandur@mutelight.org>
2013-03-09 14:11:17 -0800

    Modularize Twitter spider; get entities

--- lib/black_swan.rb
+++ lib/black_swan.rb
@@ -1,6 +1,7 @@
 # support
 require_relative "black_swan/config"
 require_relative "black_swan/error_handling"
+require_relative "black_swan/spiders/twitter"
 
 # modules
 require_relative "black_swan/assets"
---
+++ lib/black_swan/spiders/twitter.rb
@@ -0,0 +1,75 @@
+module BlackSwan::Spiders
+  class Twitter
+    def run
+      update
+      backfill
+    end
+
+    private
+
+    def backfill
+      puts "backfilling"
+      earliest = DB[:events].min("slug::bigint".lit)
+      begin
+        new, events = process_page(max_id: earliest)
+        puts "processed=#{new} earliest=#{earliest}"
+        earliest = events.count > 0 ? events.min_by { |e| e["id"] }["id"] : nil
+      end while new > 0
+    end
+
+    def expand_urls(event)
+      content = event["text"]
+      # axe Twitter's crappy shortlinks
+      if event["entities"]
+        event["entities"]["urls"].each do |url|
+          if url["expanded_url"]
+            content.sub!(url["url"], url["expanded_url"])
+          end
+        end
+      end
+      content
+    end
+
+    def process_page(options={})
+      new = 0
+      res = Excon.get(
+        "https://api.twitter.com/1/statuses/user_timeline.json",
+        expects: 200,
+        query: {
+          count:            200,
+          include_entities: "true",
+          max_id:           options[:max_id],
+          screen_name:      BlackSwan::Config.twitter_handle,
+          since_id:         options[:since_id],
+          trim_user:        "true",
+        }.reject { |k, v| v == nil })
+      events = MultiJson.decode(res.body)
+      events.each do |event|
+        next if DB[:events].first(slug: event["id"].to_s) != nil
+        new += 1
+
+        DB[:events].insert(
+          content:     expand_urls(event),
+          occurred_at: event["created_at"],
+          slug:        event["id"].to_s,
+          metadata: {
+            mention:   (event["text"] =~ /^\s*@/) != nil,
+            permalink: "https://twitter.com/#{BlackSwan::Config.twitter_handle}" +
+              "/statuses/#{event["id"]}",
+          }.hstore)
+      end
+
+      [new, events]
+    end
+
+    def update
+      puts "updating"
+      latest = DB[:events].max("slug::bigint".lit)
+      begin
+        new, events = process_page(since_id: latest)
+        puts "processed=#{new} latest=#{latest}"
+        latest = events.count > 0 ? events.max_by { |e| e["id"] }["id"] : nil
+      end while new > 0
+    end
+  end
+end
